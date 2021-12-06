a549b8cc63ca9aa23a5b0db3dccf3e5723a1a5f3
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2021-12-06 15:29:14 +0100

    Add tests

---
+++ lernejo-tester/src/test/java/fr/lernejo/tester/SomeLernejoTests.java
@@ -0,0 +1,16 @@
+package fr.lernejo.tester;
+
+import fr.lernejo.tester.api.TestMethod;
+
+public class SomeLernejoTests {
+
+	@TestMethod
+	public void ok() {}
+
+	@TestMethod
+	public void ko() {
+		throw new IllegalStateException();
+	}
+
+	public void none() {}
+}
